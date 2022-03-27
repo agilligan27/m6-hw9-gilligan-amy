b3867c0451d3d3d8c4d4430a717233e7d31e3179
Amy Gilligan <agilligan27@gmail.com>
2022-03-27 14:53:27 -0400

    Call API on form submission

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
 
-var formEl = document.querySelector('form');
+var form = document.querySelector('form');
 var locationInput = document.querySelector('input[type=text]');
 var weatherLog = document.getElementById('weather');
 
+var api = 'https://api.openweathermap.org/data/2.5/weather?'
+var apiKey = '&units=imperial&appid=e96f78fb54ab52579081833f456f29e6'
 
 
-
-formEl.onsubmit = function(e){
-    e.preventDefault()
-    fetch('https://api.openweathermap.org/data/2.5/weather?id=524901&appid=e96f78fb54ab52579081833f456f29e6')
+form.onsubmit = function(e) {
+    e.preventDefault();
+    var location = locationInput.value;
+    fetch(api + 'q=' + location + apiKey)
     .then(function(res){
-        return res.json()
+        return res.json();
     })
     .then(function(res){
         console.log(res);
     })
-    .catch(function(err){
-        console.log('Error!', err)
-    })
-    
 }
 
 
-
-
-
-// form onsubmit, formData obj
-//formEl.onsubmit = function(e) {
-  //  e.preventDefault()
-    //var formData = new FormData(e.target)
-    //console.log(formData.get('location'))
-    //console.log(Object.fromEntries(formData))
-  //}
-  
-  // form submit event listener, input elements
-  //formEl.addEventListener('submit', function(e) {
-    //e.preventDefault()
-    //var name = locationInput.value
-    //console.log("event listener", {location})
-  //})
 
 
